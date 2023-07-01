0946f3901cdf55d47d692105f30ce8caaba030fc
John Maximilian <2e0byo@gmail.com>
2023-07-01 19:26:06 +0100

    feat: interface.

---
+++ firmware/interface.py
@@ -0,0 +1,55 @@
+from hal import static_hal
+from machine import Pin, SPI, I2C, UART
+import hal
+
+
+def make_spi(**mapping):
+    return SPI(
+        baudrate=1000000,
+        polarity=0,
+        phase=0,
+        bits=8,
+        sck=translate_pin(mapping["CLK"]),
+        mosi=translate_pin(mapping["MOSI"]),
+        miso=translate_pin(mapping["MISO"]),
+        cs=translate_pin(mapping["CS"]),
+    )
+
+
+def make_i2c(**mapping):
+    return I2C(scl=translate_pin(mapping["SCL"]), sda=translate_pin(mapping["SDA"]))
+
+
+def translate_pin(pin: hal.Pin) -> Pin:
+    print(pin)
+    direction = Pin.IN if pin.input else Pin.OUT
+    special_names: dict[str, int] = {}
+    return Pin(special_names.get(pin.name, int(pin.name[2:])), direction)
+
+
+def make_pins(**mapping):
+    return {k: translate_pin(v) for k, v in mapping.items() if v not in skip_pins}
+
+
+bus_types = {
+    "SPI": make_spi,
+    "I2C": make_i2c,
+}
+
+skip_pins = ("EN", "TXD0/IO1", "RXD0/IO3", "IO0")
+skip_buses = ("MCU_CTL", "SPI")
+
+buses = {
+    k: bus_types.get(k, make_pins)(**v)
+    for k, v in static_hal.items()
+    if k not in skip_buses
+}
+
+
+def psu(state: "bool | None" = None) -> bool:
+    p = buses["ATX_CTL"]["PWR_ON"]
+    if state:
+        p.init(p.OUT, value=0)
+    elif state is not None:
+        p.init(p.IN)
+    return not p.value()
