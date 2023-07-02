739abb991134c3dbf378fc2b5213348ab737d226
John Maximilian <2e0byo@gmail.com>
2023-07-02 23:14:42 +0100

    feat: jsonrpc.

---
+++ firmware/jsonrpc.py
@@ -0,0 +1,110 @@
+import json
+
+
+class ParseError(Exception):
+    ...
+
+
+class InvalidRequest(Exception):
+    ...
+
+
+class MethodNotFound(Exception):
+    ...
+
+
+class InvalidParams(Exception):
+    ...
+
+
+class RPC:
+    PARSE_ERROR = -32700
+    INVALID_REQUEST = -32600
+    METHOD_NOT_FOUND = -32601
+    INVALID_PARAMS = -32602
+    INTERNAL_ERROR = -32603
+    SERVER_ERROR = -32000
+
+    def __init__(self, methods: "dict | None" = None):
+        self.methods = {}
+        if methods:
+            for name, method in methods.items():
+                self.register(name, method)
+
+    def register(self, name, method):
+        self.methods[name] = method
+
+    def parse(self, raw_data: str) -> dict:
+        try:
+            data = json.loads(raw_data)
+        except Exception as e:
+            raise ParseError() from e
+        return data
+
+    def extract_params(self, data: dict) -> None:
+        params = data.get("params")
+        if params:
+            if type(params) not in {dict, list}:
+                raise InvalidRequest()
+            data["kwargs"] = params if isinstance(params, dict) else {}
+            data["args"] = params if isinstance(params, list) else []
+            del data["params"]
+
+    def validate(self, data):
+        if not all(data.get(x) for x in ("jsonrpc", "method")):
+            raise InvalidRequest()
+
+        if data.get("jsonrpc", None) != "2.0":
+            raise InvalidRequest("Missing or invalid version.")
+
+    def error(self, e: Exception, id: "str | None" = None) -> dict:
+        code, default_msg = {
+            ParseError: (self.PARSE_ERROR, "Parse error"),
+            InvalidRequest: (self.INVALID_REQUEST, "Invalid Request"),
+            MethodNotFound: (self.METHOD_NOT_FOUND, "Method not found"),
+            InvalidParams: (self.INVALID_PARAMS, "Invalid params"),
+        }.get(type(e), (self.INTERNAL_ERROR, "Internal error"))
+        return {
+            "jsonrpc": "2.0",
+            "id": id,
+            "error": {"code": code, "message": str(e) or default_msg},
+        }
+
+    def call_method(self, data: dict) -> "dict | str | int":
+        try:
+            method = self.methods[data["method"]]
+        except KeyError as e:
+            raise MethodNotFound("Method not found") from e
+        return method(*data["args"], **data["kwargs"])
+
+    def _handle_entry(self, data: dict):
+        if not isinstance(data, dict):
+            return self.error(InvalidRequest())
+        id = None
+        try:
+            self.extract_params(data)
+            id = data.get("id")
+            self.validate(data)
+            result = self.call_method(data)
+            if id:
+                return {"jsonrpc": "2.0", "result": result, "id": id}
+            else:
+                return None
+        except Exception as e:
+            return self.error(e, id)
+
+    def _handle_packet(self, packet: str) -> "str | None":
+        try:
+            data = self.parse(packet)
+            if isinstance(data, list):
+                if not data:
+                    raise InvalidRequest()
+                return [self._handle_entry(x) for x in data]
+            else:
+                return self._handle_entry(data)
+        except Exception as e:
+            return self.error(e)
+
+    def handle_packet(self, packet: str):
+        resp = self._handle_packet(packet)
+        return json.dumps(resp) if resp else None
---
+++ test_firmware.py
@@ -0,0 +1,144 @@
+from firmware.jsonrpc import RPC
+from json import dumps, loads
+import pytest
+
+
+@pytest.fixture
+def rpc(mocker):
+    return RPC(methods={"foo": mocker.Mock(), "bar": mocker.Mock()})
+
+
+def test_invalid_version_returns_error(rpc):
+    request = dumps({"jsonrpc": "1.0", "method": "foo", "id": "1"})
+    assert loads(rpc.handle_packet(request)) == {
+        "jsonrpc": "2.0",
+        "error": {"code": -32600, "message": "Missing or invalid version."},
+        "id": "1",
+    }
+
+
+def test_missing_method_returns_error(rpc):
+    request = dumps({"jsonrpc": "2.0", "method": "foobar", "id": "1"})
+    assert loads(rpc.handle_packet(request)) == {
+        "jsonrpc": "2.0",
+        "error": {"code": -32601, "message": "Method not found"},
+        "id": "1",
+    }
+
+
+def test_invalid_json_returns_error(rpc):
+    request = '{"jsonrpc": "2.0", "method": "foobar, "params": "bar", "baz]'
+    assert loads(rpc.handle_packet(request)) == {
+        "jsonrpc": "2.0",
+        "error": {"code": -32700, "message": "Parse error"},
+        "id": None,
+    }
+
+
+def test_invalid_request_returns_error(rpc):
+    request = dumps({"jsonrpc": "2.0", "method": 1, "params": "bar"})
+    assert loads(rpc.handle_packet(request)) == {
+        "jsonrpc": "2.0",
+        "error": {"code": -32600, "message": "Invalid Request"},
+        "id": None,
+    }
+
+
+def test_batch_invalid_json_returns_error(rpc):
+    request = """[
+  {"jsonrpc": "2.0", "method": "sum", "params": [1,2,4], "id": "1"},
+  {"jsonrpc": "2.0", "method"
+]"""
+    assert loads(rpc.handle_packet(request)) == {
+        "jsonrpc": "2.0",
+        "error": {"code": -32700, "message": "Parse error"},
+        "id": None,
+    }
+
+
+def test_batch_empty_array_returns_error(rpc):
+    request = "[]"
+    assert loads(rpc.handle_packet(request)) == {
+        "jsonrpc": "2.0",
+        "error": {"code": -32600, "message": "Invalid Request"},
+        "id": None,
+    }
+
+
+def test_non_rpc_array_returns_error(rpc):
+    request = "[1,2,3]"
+    assert loads(rpc.handle_packet(request)) == [
+        {
+            "jsonrpc": "2.0",
+            "error": {"code": -32600, "message": "Invalid Request"},
+            "id": None,
+        },
+        {
+            "jsonrpc": "2.0",
+            "error": {"code": -32600, "message": "Invalid Request"},
+            "id": None,
+        },
+        {
+            "jsonrpc": "2.0",
+            "error": {"code": -32600, "message": "Invalid Request"},
+            "id": None,
+        },
+    ]
+
+
+def test_single_batch_invalid_returns_error(rpc):
+    request = "[1]"
+    assert loads(rpc.handle_packet(request)) == [
+        {
+            "jsonrpc": "2.0",
+            "error": {"code": -32600, "message": "Invalid Request"},
+            "id": None,
+        },
+    ]
+
+
+def test_positional_params(rpc):
+    rpc.methods["foo"].return_value = 17
+    request = dumps({"jsonrpc": "2.0", "method": "foo", "params": [1, 2, 3], "id": 1})
+    assert loads(rpc.handle_packet(request)) == {
+        "jsonrpc": "2.0",
+        "result": 17,
+        "id": 1,
+    }
+    rpc.methods["foo"].assert_called_once_with(1, 2, 3)
+
+
+def test_named_params(rpc):
+    rpc.methods["foo"].return_value = 19
+    request = dumps(
+        {"jsonrpc": "2.0", "method": "foo", "params": {"a": 1, "b": 2}, "id": 1}
+    )
+    assert loads(rpc.handle_packet(request)) == {
+        "jsonrpc": "2.0",
+        "result": 19,
+        "id": 1,
+    }
+    rpc.methods["foo"].assert_called_once_with(a=1, b=2)
+
+
+def test_notification(rpc):
+    request = dumps({"jsonrpc": "2.0", "method": "foo", "params": [1, 2, 3]})
+    assert not rpc.handle_packet(request)
+    assert rpc.methods["foo"].called_once_with(1, 2, 3)
+
+
+def test_batch_call(rpc):
+    rpc.methods["foo"].return_value = "fooresult"
+    rpc.methods["bar"].return_value = "barresult"
+    request = dumps(
+        [
+            {"jsonrpc": "2.0", "method": "foo", "params": [1, 2, 3], "id": 1},
+            {"jsonrpc": "2.0", "method": "bar", "params": {"a": "b"}, "id": 2},
+        ]
+    )
+    assert loads(rpc.handle_packet(request)) == [
+        {"jsonrpc": "2.0", "result": "fooresult", "id": 1},
+        {"jsonrpc": "2.0", "result": "barresult", "id": 2},
+    ]
+    rpc.methods["foo"].assert_called_once_with(1, 2, 3)
+    rpc.methods["bar"].assert_called_once_with(a="b")
