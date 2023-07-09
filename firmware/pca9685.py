606e9aee29f8ea0b143cfc02b9130c1eac4f9119
John Maximilian <2e0byo@gmail.com>
2023-07-09 18:31:51 +0100

    fix: fixups for pca6985.

--- firmware/pca9685.py
+++ firmware/pca9685.py
@@ -46,6 +46,9 @@ class Controller:
         self.entered = False
         self._reset()
 
+    def frequency(self, val_hz: "int | None" = None) -> int:
+        raise NotImplementedError()
+
 
 class Channel:
     def __init__(self, controller: Controller, channel: int, offset: int):
@@ -68,17 +71,19 @@ class Channel:
         return period
 
     def percent_brightness(self, value: "float | None" = None) -> float:
-        if value:
+        if value is not None:
             value = max(0, min(1, value))
             self.brightness(round(value * 4096))
-        return self.brightness() / 4096
+        brightness = self.brightness()
+        return brightness / 4096 if brightness != 4095 else 1
 
 
 class PCA9685(Controller):
-    def __init__(self, i2c: I2C, address: int = 0x40):
+    def __init__(self, i2c: I2C, address: int = 0x40, clk_hz: int = 25_000_000):
         self.i2c = i2c
         self.address = address
         self.channels = [Channel(self, i, i * 256) for i in range(16)]
+        self.clk_hz = clk_hz
         super().__init__()
 
     def _write(self, address: int, data: bytes):
@@ -106,3 +111,30 @@ class PCA9685(Controller):
             int.from_bytes(on_t, "little"),
             int.from_bytes(off_t, "little"),
         )
+
+    def _frequency_from_prescale(self, prescale: int) -> int:
+        return round(self.clk_hz / (4096 * (prescale + 1)))
+
+    def frequency(self, val_hz: "int | None" = None) -> int:
+        if val_hz is not None:
+            prescale = round(self.clk_hz / (4096 * val_hz)) - 1
+            print(prescale)
+            if prescale < 3:
+                raise ValueError(
+                    f"Cannot set such a high frequency: {val_hz}, maximum is {self._frequency_from_prescale(3)} Hz"
+                )
+            if prescale > 0xFF:
+                raise ValueError(
+                    f"Cannot set such a low frequency: {val_hz}, minimum is {self._frequency_from_prescale(0xff)} Hz"
+                )
+            mode1 = self._read(0, 1)
+            self._write(
+                0,
+                ((int.from_bytes(mode1, "little") & 0x7F) | 0x10).to_bytes(1, "little"),
+            )  # put to sleep before changing freq
+            self._write(0xFE, prescale.to_bytes(1, "little"))
+            self._write(0, mode1)  # restore old state
+            time.sleep_us(5)
+
+        prescale = int.from_bytes(self._read(0xFE, 1), "little")
+        return self._frequency_from_prescale(prescale)
