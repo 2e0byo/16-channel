68e714ab7d963dabcf9fe007e0dc76386b34246f
John Maximilian <2e0byo@gmail.com>
2023-07-09 18:32:39 +0100

    feat: network.

---
+++ firmware/conn.py
@@ -0,0 +1,14 @@
+from env import SSID, KEY
+
+
+def do_connect():
+    import network
+
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print("connecting to network...")
+        sta_if.active(True)
+        sta_if.connect(SSID, KEY)
+        while not sta_if.isconnected():
+            pass
+    print("network config:", sta_if.ifconfig())
